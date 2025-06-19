d04731463c44ffc525e584ee99949fe90a142334
Balint Szikora <chamaselion@gmail.com>
2025-06-19 15:50:43 +0200

    Corrected wrong animals

--- ex00/sources/main.cpp
+++ ex00/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bszikora <bszikora@student.42helbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 17:35:45 by bszikora          #+#    #+#             */
-/*   Updated: 2025/05/25 16:08:50 by bszikora         ###   ########.fr       */
+/*   Updated: 2025/06/19 14:58:08 by bszikora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,16 +30,34 @@
 // 	return 0;
 // }
 
+// int main()
+// {
+// const Animal* meta = new Animal();
+// const Animal* j = new Dog();
+// const Animal* i = new Cat();
+
+// std::cout << j->getType() << " " << std::endl;
+// std::cout << i->getType() << " " << std::endl;
+// i->makeSound(); //will output the cat sound!
+// j->makeSound();
+// meta->makeSound();
+
+// return 0;
+// }
+
 int main()
 {
-const Animal* meta = new Animal();
-const Animal* j = new Dog();
-const WrongAnimal* i = new WrongCat();
-std::cout << j->getType() << " " << std::endl;
-std::cout << i->getType() << " " << std::endl;
-i->makeSound(); //will output the cat sound!
-j->makeSound();
-meta->makeSound();
+	const WrongAnimal* wrongAnimal = new WrongAnimal();
+	wrongAnimal->makeSound();
+	const WrongAnimal* wrongCatAsAnimal = new WrongCat();
+	wrongCatAsAnimal->makeSound();
+	
+	const WrongCat* wrongCat = new WrongCat();
+	wrongCat->makeSound();
 
-return 0;
+	delete wrongAnimal;
+	delete wrongCatAsAnimal;
+	delete wrongCat;
+
+    return 0;
 }
